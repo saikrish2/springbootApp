f2643f7a2a9d498e3b8fbdce1ee1ed5d9cede176
sandhyavatrapu <vatrapusandhya2@gmail.com>
2023-03-26 17:37:23 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/springbootApp/SampleAppplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.springbootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAppplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAppplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/springbootApp/SampleAppplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.springbootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAppplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
